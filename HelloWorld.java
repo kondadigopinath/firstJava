1c9737415619b311b7ea99f7d3502ea017634e73
kondadigopinath <60908346+kondadigopinath@users.noreply.github.com>
2020-02-12 09:53:06 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello,New World on 12th feb. 14:37 hrs. 2019");
+    System.out.println("Hello World on 12th feb");
   }
 }
